4268c35ec1bbe64702a8f4d7c05dd971745c3716
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-05-13 16:44:00 +0100

    2

---
+++ ejercicio1.js
@@ -0,0 +1,34 @@
+//Ejercicio 1
+//1. Define tu superhéroe favorito:
+//Crea una variable llamada miSuperheroeFavorito y asigna el valor "Hulk".
+
+var miSuperheroeFavorito = "Hulk";
+
+//2. Define un número:
+//Crea una variable llamada numeroFavorito y asigna el valor 50.
+
+var numeroFavorito = 50;
+
+//3. Define las dimensiones:
+//Crea una variable llamada altura y asigna el valor 5.
+
+var altura = 5;
+
+//Crea una variable llamada longitud y asigna el valor 10.
+
+var longitud =10;
+
+//4. Calcula la suma:
+//Crea una variable llamada suma y asigna el valor de altura + longitud.
+
+var suma = altura + longitud;
+
+///5. Imprime los resultados:
+//Muestra todas las variables por consola. 
+
+console.log("Mi superhéroe favorito es", miSuperheroeFavorito);
+console.log("Mi número favorito es:", numeroFavorito)
+console.log("La altura es", altura);
+console.log("La longitud es", longitud);
+console.log("La suma de la altura y de la longitud es", suma);
+
---
+++ ejercicio2.js
@@ -0,0 +1,19 @@
+//1. Luke Skywalker cumple años:
+//Modifica el siguiente código JavaScript para actualizar la edad de Luke Skywalker a 25 años.
+
+const jedi = {nombre: "Luke Skywalker", edad: 19};
+
+// Tu código para cambiar la edad aquí...
+
+jedi.edad = 30;
+
+console.log(jedi);
+
+//2. Presentación al estilo Leia Organa:
+//- Crea tres variables con la siguiente información:
+//nombre: "Leia"
+//apellido: "Organa"
+//edad: 20
+//- Muestra un mensaje por consola que siga la siguiente estructura:
+//"Soy Leia Organa, tengo 20 años y soy una princesa de Alderaan."
+//- Utiliza la concatenación para ello.
