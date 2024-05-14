f1a4f29c5a26e955eba8bd2eabaf455e663b71bf
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-05-14 21:21:28 +0100

    7

---
+++ ejercicio7.js
@@ -0,0 +1,17 @@
+//Completa esta función para que, al recibir dos números por argumento,
+//te devuelva por consola el más alto de los dos.
+
+function greaterNumber(numberOne , numberTwo) {
+    if (numberOne > numberTwo) {
+        return numberOne;
+    }
+
+    else {
+
+       return numberTwo; 
+    }
+
+}
+    console.log(greaterNumber(6,8));
+
+  
---
+++ ejercicio8.js
@@ -0,0 +1,23 @@
+/* Buscar la palabra más larga: Completa la función que tomando un array 
+de strings como argumento devuelva el más largo, en caso de que dos strings
+ tenga la misma longitud deberá devolver el primero.
+
+Puedes usar este array para probar tu función: */
+
+const avengers = ['Hulk', 'Thor', 'Iron Man', 'Captain M.', 'Spiderman', 'Captain A.'];
+
+function findLongestWord(stringList) {
+  let longestWord = ''; 
+  
+  for (let i = 0; i < stringList.length; i++) {
+
+    if (stringList[i].length >= longestWord.length) {
+      
+      longestWord = stringList[i];
+    }
+  }
+  
+  return longestWord;
+}
+
+console.log(findLongestWord(avengers));
