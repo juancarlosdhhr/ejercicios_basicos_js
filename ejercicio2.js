54608ab78271916394cb3779da870428cdf18b9d
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-05-14 18:50:02 +0100

    4

--- ejercicio2.js
+++ ejercicio2.js
@@ -5,6 +5,7 @@ const jedi = {nombre: "Luke Skywalker", edad: 19};
 
 // Tu código para cambiar la edad aquí...
 
+
 jedi.edad = 30;
 
 console.log(jedi);
@@ -17,3 +18,54 @@ console.log(jedi);
 //- Muestra un mensaje por consola que siga la siguiente estructura:
 //"Soy Leia Organa, tengo 20 años y soy una princesa de Alderaan."
 //- Utiliza la concatenación para ello.
+
+var nombre = "Leia";
+var apellido = "Organa";
+var edad = 20;
+
+console.log("Soy " + nombre + " " + apellido + ", tengo " + edad + " años y soy una princesa de Alderaan.");
+
+
+/* 3. Calculando el coste total de sables de luz:
+
+- Obtén el precio total de dos sables de luz: "Shoto de Yoda" y "sable de Darth Vader".
+
+- Imprime el resultado por consola utilizando console.log. */       
+
+
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+
+// Tu código para calcular el precio total aquí...
+
+const precioTotal = sable1.precio + sable2.precio;
+
+console.log("La suma de los precios de los sables es de " + precioTotal)
+
+/* 4. Actualizando el precio final de las naves:
+
+- Modifica el valor de la variable global precioBaseGlobal a 25.000 créditos.
+
+- Actualiza el precio final (precioFinal) de dos naves ("Ala-X" y "Halcón Milenario") sumando el valor de precioBaseGlobal a su precio base (precioBase). */
+
+let precioBaseGlobal = 10000;
+
+// Cambia el precio base en esta linea modificando la variable
+
+precioBaseGlobal = 25000;
+
+const nave1 = {nombre: "Ala-X", precioBase: 50000, precioFinal: 60000};
+const nave2 = {nombre: "Halcón Milenario", precioBase: 70000, precioFinal: 80000};
+
+// Tu código para actualizar el precio final de cada nave aquí...
+
+nave1.precioFinal = nave1.precioBase + precioBaseGlobal;
+nave2.precioFinal = nave2.precioBase + precioBaseGlobal;
+
+console.log(nave1)
+console.log(nave2)
+
+
+
+
+
