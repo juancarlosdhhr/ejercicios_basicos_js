c791740502f155d13006f8e99baae70e63beb38f
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-06-16 02:14:32 +0100

    1

---
+++ ejercicio13.js
@@ -0,0 +1,42 @@
+/* Ejercicio 13
+
+Buscador de nombres: Crea una función que reciba por parámetro un array y el valor que desea comprobar que 
+existe dentro de dicho array. Esta función comprueba si existe el elemento, y en caso de que exista nos 
+devuelve un true  y la posición de dicho elemento y por la contra un false.
+
+Puedes usar este array para probar tu función: */
+
+const names = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+  ];
+  function nameFinder(nameList) {
+    // Completar
+  }
+
+
+  function nameFinder(nameList, nameToFind) {
+ 
+    for (let i=0; i < nameList.length; i++) {
+        if (nameList[i] === nameToFind) {
+
+            return [true, i];
+        }
+
+
+    }
+return [false];
+
+  }
+
+  console.log(nameFinder(names, 'Tony'));
+
